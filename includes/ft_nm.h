c0c1ce9e09262221bf017dc33f7669a970844ba3
user42 <Vous@exemple.com>
2020-10-05 10:57:42 +0200

    Yes

--- includes/ft_nm.h
+++ includes/ft_nm.h
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #ifndef __FT_NM_H__
 # define MALLOC_ERR 1
 
@@ -18,16 +17,113 @@
 # define FLAG_Q 2
 # define FLAG_R 4
 # define FLAG_S 8
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/mman.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+/*
+ ** MACH-O FORMAT
+ */
 
-typedef struct s_nm
+typedef unsigned char				uint8_t;
+typedef unsigned short int			uint16_t;
+# ifndef __uint32_t_defined
+typedef unsigned int				uint32_t;
+#  define __uint32_t_defined
+# endif
+# if __WORDSIZE == 64
+typedef unsigned long int			uint64_t;
+# else
+typedef unsigned long long int		 uint64_t;
+# endif
+
+typedef int32_t					integer_t;
+typedef integer_t				cpu_type_t;
+typedef integer_t				cpu_subtype_t;
+typedef integer_t				cpu_threadtype_t;
+
+
+
+typedef struct		s_nm
 {
-	char **files;
-	int file_count;
-	int flag;
-} t_nm;
+	char			**files;
+	int				file_count;
+	int				flag;
+}					t_nm;
 
-t_nm g_nm;
+t_nm				g_nm;
 
-//int			get_args(int ac,char ** av);
+typedef struct		mach_header {
+	uint32_t		magic;
+	cpu_type_t		cputype;
+	cpu_subtype_t	cpusubtype;
+	uint32_t		filetype;
+	uint32_t		ncmds;
+	uint32_t		sizeofcmds;
+	uint32_t		flags;
+}					t_mach_header;
+
+# define MH_MAGIC 0xfeedface
+# define MH_CIGAM 0xcefaedfe
+
+typedef struct		mach_header_64 {
+	uint32_t		magic;
+	cpu_type_t		cputype;
+	cpu_subtype_t	cpusubtype;
+	uint32_t		filetype;
+	uint32_t		ncmds;
+	uint32_t		sizeofcmds;
+	uint32_t		flags;
+	uint32_t		reserved;
+}					t_mach_header_64;
+
+#define MH_MAGIC_64 0xfeedfacf
+#define MH_CIGAM_64 0xcffaedfe
+
+typedef struct		load_command {
+	uint32_t		cmd;           /* type of load command */
+	uint32_t		cmdsize;       /* total size of command in bytes */
+}					t_load_command;
+
+
+# define LC_SYMTAB 0x2
+
+typedef struct		symtab_command {
+	uint32_t		cmd;            /* LC_SYMTAB */
+	uint32_t		cmdsize;        /* sizeof(struct symtab_command) */
+	uint32_t		symoff;         /* symbol table offset */
+	uint32_t		nsyms;          /* number of symbol table entries */
+	uint32_t		stroff;         /* string table offset */
+	uint32_t		strsize;        /* string table size in bytes */
+}					t_symtab_command;
+
+typedef struct		nlist {
+	union {
+#ifndef __LP64__
+		char *		n_name;   /* for use when in-core */
+#endif
+		uint32_t	n_strx;        /* index into the string table */
+	}				n_un;
+	uint8_t			n_type;         /* type flag, see below */
+	uint8_t			n_sect;         /* section number or NO_SECT */
+	int16_t			n_desc;         /* see <mach-o/stab.h> */
+	uint32_t		n_value;       /* value of this symbol (or stab offset) */
+}					t_nlist;
+
+/*
+ * This is the symbol table entry structure for 64-bit architectures.
+ */
+typedef struct		nlist_64 {
+	union {
+		uint32_t	n_strx; /* index into the string table */
+	}				n_un;
+	uint8_t			n_type;        /* type flag, see below */
+	uint8_t			n_sect;        /* section number or NO_SECT */
+	uint16_t		n_desc;       /* see <mach-o/stab.h> */
+	uint64_t		n_value;      /* value of this symbol (or stab offset) */
+}					t_nlist_64;
+
 
 #endif
--- srcs/ft_nm/ft_nm.c
+++ srcs/ft_nm/ft_nm.c
@@ -12,9 +12,61 @@
 
 #include <ft_nm.h>
 
+int nm(char *ptr)
+{
+	unsigned int magic_number;
+
+	magic_number = *(int *)ptr;
+	if (magic_number == MH_MAGIC_64)
+	{
+		write(1,"6444",4);
+	}
+	if (magic_number == MH_MAGIC)
+	{
+		write(1,"3222",4);
+	}
+	return (0);
+}
+int handle_file(char *filename)
+{
+	int				fd;
+	char			*ptr;
+	struct stat		buf;
+
+	if ((fd = open(filename, O_RDONLY)) < 0)
+		return (EXIT_FAILURE);
+	if (fstat(fd, &buf) < 0)
+		return (EXIT_FAILURE);
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0))
+		== MAP_FAILED)
+		return (EXIT_FAILURE);
+	nm(ptr);
+	if (munmap(ptr, buf.st_size) < 0)
+		return (EXIT_FAILURE);
+	close(fd);
+	return (0);
+}
+
 int main(int ac, char **av)
 {
-(void)ac;
-(void)av;
-//	get_args(ac, av);
+	int		i;
+
+	i = 0;
+	if (ac == 1)
+	{
+		if (handle_file("./a.out") == EXIT_FAILURE)
+		{
+			write(2, "ERROR", 5);
+			return (1);
+		}
+	}
+	else
+	{
+		while (ac-- > 1)
+		{
+			if ((handle_file(av[++i])) == EXIT_FAILURE)
+				write(2, "ERROR", 5);
+		}
+	}
+	return (0);
 }
